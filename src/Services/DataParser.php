5adba9e34ae476d5cc2397e9c4db9aa822ff6dee
RiZoR <nathan.charvy@gmail.com>
2018-02-02 16:00:26 +0100

    finish the parsing service

--- src/Services/DataParser.php
+++ src/Services/DataParser.php
@@ -4,7 +4,21 @@ namespace App\Services;
 
 class DataParser
 {
-	public function formatBody($body){
-		
-	}
+    // Method used to format the return body of the request
+    // Embeds the commits list passed in parameter and uses
+    // the second parameter (timestamp) to register wanted
+    // information to the data
+    public function formatBody($body, $timestamp){
+        if(null === $timestamp){
+            $timestamp = time();
+        }
+        $data = array(
+            "year" => date("Y", $timestamp),
+            "week" => date("W", $timestamp),
+            "count" => count($body),
+            "commits" => $body
+        );
+
+        return $data;
+    }
 }
